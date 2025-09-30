312faeaddd0eef98c5941255129e31a6994d002a
Brian <Brian@automatize.com>
2025-09-30 13:48:03 -0500

    change port

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
